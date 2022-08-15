be9c88938e731f041d41efbf9989d40ac64e5c40
Grace K Foster <gkfoster15@gmail.com>
2022-08-15 10:05:55 -0400

    Add: event loop & window size

--- game.py
+++ game.py
@@ -1,17 +1,26 @@
-import pygame
-from pygame.locals import *
+import sys, pygame
+from pygame.locals import * 
 
-# pygame window design / import
+# initializing pygame
+pygame.init()
+
+# pygame window design
 background_color = (234, 212, 252)
-screen = pygame.display.set_mode((900, 800))
+screen = pygame.display.set_mode((700, 600))
 pygame.display.set_caption('Mushroom Mage')
 screen.fill(background_color)
 pygame.display.flip()
 
+# var to keep the main loop
 running = True
 
 while running:
     for event in pygame.event.get():
+        if event.type == KEYDOWN:
 
-        if event.type == pygame.QUIT:
+            if event.key == K_ESCAPE:
+                running = False
+
+        elif event.type == pygame.QUIT:
             running = False
+
